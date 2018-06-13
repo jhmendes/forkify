5b39b4f20ef07453c95de9c47eca952df1492183
John Mendes <johnhenrymendes@gmail.com>
2018-06-12 21:11:53 -0400

    API key setup and tested api calls to food2fork.com using Axios, ready to begin building app

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,21 @@
-// Global app controller
+import axios from 'axios';
 
-// import str from './models/Search';
-// //import { add as a, multiply as m, ID } from './views/searchView';
-// import * as searchView from './views/searchView';
-// console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}  and then ${str}`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '33321ee2df8b9bd14d62e171ce07d7de';
+
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+   
+}
+  
+getResults('pizza'); 
+
+//API KEY for food2fork 33321ee2df8b9bd14d62e171ce07d7de
+//http://food2fork.com/api/search 
+
